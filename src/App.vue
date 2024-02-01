91d98675422c28cd1931696164b5d723702c331a
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-02-01 16:56:16 +0100

    added header structure

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,73 @@
 </script>
 
 <template>
-  <h1>ciao</h1>
+  <div class="main-container">
+    <header>
+      <nav>
+        <img src="../src/assets/img/dc-logo.png" alt="">
+        <ul class="links-list">
+          <li>Ciao be</li>
+          <li>Ciao be</li>
+          <li>Ciao be</li>
+          <li>Ciao be</li>
+          <li>Ciao be</li>
+          <li>Ciao be</li>
+          <li>Ciao be</li>
+          <li>Ciao be</li>
+          <li>Ciao be</li>
+          <li>Ciao be</li>
+        </ul>
+      </nav>
+
+    </header>
+
+  </div>
 </template>
 
-<style></style>
+<style>
+/*CSS RESET*/
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+/*UTILS*/
+
+
+
+/*GENERICS*/
+
+header {
+  height: 150px;
+  background-color: forestgreen;
+}
+
+.main-container {
+  margin: 50px 0px;
+
+}
+
+header nav {
+  width: 70%;
+  margin: 0 auto;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.links-list {
+  display: flex;
+  list-style: none;
+  gap: 20px;
+  height: 100%;
+  align-items: center;
+}
+
+.links-list li {
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+</style>
