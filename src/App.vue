e03dcaf9b914e7c3684fc802d78ff3549b38f4a7
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-02-01 17:40:38 +0100

    added dc-info structure

--- src/App.vue
+++ src/App.vue
@@ -19,8 +19,41 @@
           <li>Ciao be</li>
         </ul>
       </nav>
-
     </header>
+    <main>
+      <!--JUMBOTRON-->
+      <div class="jumbotron">
+        <div class="jumbotron-card">
+          <p>--Content goes here--</p>
+        </div>
+      </div>
+      <!--BONUS-->
+      <div class="dc-info">
+        <div class="info-links">
+          <div class="links-card">
+            <img src="../src/assets/img/buy-comics-digital-comics.png" alt="">
+            <p>DIGITAL COMICS</p>
+          </div>
+          <div class="links-card">
+            <img src="../src/assets/img/buy-comics-merchandise.png" alt="">
+            <p>DIGITAL COMICS</p>
+          </div>
+          <div class="links-card">
+            <img src="../src/assets/img/buy-comics-shop-locator.png" alt="">
+            <p>DIGITAL COMICS</p>
+          </div>
+          <div class="links-card">
+            <img src="../src/assets/img/buy-comics-subscriptions.png" alt="">
+            <p>DIGITAL COMICS</p>
+          </div>
+          <div class="links-card">
+            <img src="../src/assets/img/buy-dc-power-visa.svg" alt="">
+            <p>DIGITAL COMICS</p>
+          </div>
+        </div>
+      </div>
+
+    </main>
 
   </div>
 </template>
@@ -46,9 +79,9 @@ header {
 
 .main-container {
   margin: 50px 0px;
-
 }
 
+/*HEADER*/
 header nav {
   width: 70%;
   margin: 0 auto;
@@ -70,5 +103,45 @@ header nav {
   height: 100%;
   display: flex;
   align-items: center;
+}
+
+/*MAIN*/
+.jumbotron {
+  height: 150px;
+  background-color: black;
+  color: white;
+}
+
+.jumbotron-card {
+  width: 70%;
+  height: 100%;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.dc-info {
+  height: 200px;
+  background-color: lightblue;
+  display: flex;
+}
+
+.info-links {
+  display: flex;
+  width: 70%;
+  margin: 0 auto;
+  padding: 50px 0px;
+  gap: 20px;
+}
+
+.info-links img {
+  width: 50px;
+}
+
+.links-card {
+  display: flex;
+  align-items: center;
+  gap: 10px;
 }
 </style>
